184dfc43368702cee8dc95372df39578a72b632d
Max <maxime.gardere.mg@gmail.com>
2025-02-16 18:41:23 +0100

    Combat "working"

--- Assets/Characters/MainCharacter/MainCharacter.cs
+++ Assets/Characters/MainCharacter/MainCharacter.cs
@@ -9,7 +9,7 @@ public class MainCharacter : Character
     void Update()
     {
         Vector2 inputs = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        CharacterController.Move(inputs.X0Y() * (speed.value * Time.deltaTime));
+        CharacterController.Move(inputs.X0Y().normalized * (speed.value * Time.deltaTime));
         
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Vector3 intersection = ray.origin + ray.direction * Mathf.Abs(ray.origin.y / ray.direction.y);
