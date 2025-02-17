15bdb301ef65fe210891b373bde15c0d5ea0e9c3
Max <maxime.gardere.mg@gmail.com>
2025-02-17 20:08:49 +0100

    Mummy attack anim

--- Assets/Characters/Enemies/Mummy/Mummy.cs
+++ Assets/Characters/Enemies/Mummy/Mummy.cs
@@ -10,9 +10,22 @@ public class Mummy : Enemy
         if (!target) return;
         Vector3 relativeTargetPosition = target.transform.position - transform.position;
         if (relativeTargetPosition.magnitude > detectionRadius) return;
+
+        TryDash(Vector3.zero);
         
         transform.forward = relativeTargetPosition;
         CharacterController.Move(relativeTargetPosition.normalized * (math.remap(4.0f, 8.0f, -0.125f, 1.0f, relativeTargetPosition.magnitude) * speed.Get() * Time.deltaTime));
     }
-    
+
+    protected override void DashStart()
+    {
+        base.DashStart();
+        Animator.SetBool("Attack", true);
+    }
+
+    protected override void DashEnd()
+    {
+        base.DashEnd();
+        Animator.SetBool("Attack", false);
+    }
 }
