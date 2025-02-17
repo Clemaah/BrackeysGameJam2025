766070820b64b96e67a37eb427c5711b82b9208d
Cécile Cayèré <cecile.cayere@gmail.com>
2025-02-17 20:00:38 +0100

    Maps variation

--- Assets/Features/LevelGeneration/RoomBehaviour.cs
+++ Assets/Features/LevelGeneration/RoomBehaviour.cs
@@ -7,6 +7,33 @@ public class RoomBehaviour : MonoBehaviour
 
     public GameObject[] walls;
     public GameObject[] doors;
+    public GameObject[] angleWalls;
+
+    private void Start() {
+        foreach (GameObject angleWall in angleWalls) {
+            int random = Random.Range(0, 3);
+
+            if (random == 0) {
+                angleWall.transform.GetChild(0).gameObject.SetActive(true);
+                angleWall.transform.GetChild(1).gameObject.SetActive(false);
+                angleWall.transform.GetChild(2).gameObject.SetActive(false);
+            }
+
+            else if (random == 1)
+            {
+                angleWall.transform.GetChild(0).gameObject.SetActive(false);
+                angleWall.transform.GetChild(1).gameObject.SetActive(true);
+                angleWall.transform.GetChild(2).gameObject.SetActive(false);
+            }
+
+            else if (random == 2)
+            {
+                angleWall.transform.GetChild(0).gameObject.SetActive(false);
+                angleWall.transform.GetChild(1).gameObject.SetActive(true);
+                angleWall.transform.GetChild(2).gameObject.SetActive(true);
+            }
+        }
+    }
 
     public void UpdateRoom(bool[] status) {
         for(int i = 0; i < status.Length; i++) {
