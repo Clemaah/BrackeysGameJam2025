a9f13c5f6f1159ca59d993ede78deb7b34d8f36d
Max <maxime.gardere.mg@gmail.com>
2025-02-21 03:09:17 +0100

    Car controller

--- Assets/Characters/MainCharacter/MainCharacter.cs
+++ Assets/Characters/MainCharacter/MainCharacter.cs
@@ -8,6 +8,7 @@ public class MainCharacter : Character
     public static MainCharacter Instance { get; private set; }
 
     public FloatValue speedMultiplierWhileShooting;
+    public FloatValue controller;
     
     public ProjectileLauncher projectileLauncher;
 
@@ -40,25 +41,37 @@ public class MainCharacter : Character
             
             if (GameManager.Instance.Paused) return;
         }
+
+        bool carController = controller.Get() > 0.5;
+        _isShooting = Input.GetMouseButton(0);
         
         
         Vector2 inputs = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        inputs = Quaternion.Euler(0, 0, -Camera.main.transform.rotation.eulerAngles.y) * inputs;
+        if (!carController)
+            inputs = Quaternion.Euler(0, 0, -Camera.main.transform.rotation.eulerAngles.y) * inputs;
 
         if (inputs.magnitude > 0.1f 
             && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.LeftShift)) 
             && TryDash(characterController.velocity.X0Z())) 
             return;
-        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 intersection = ray.origin + ray.direction * Mathf.Abs((ray.origin.y - projectileLauncher.transform.position.y) / ray.direction.y);
-        transform.forward = (intersection - transform.position).X0Z();
+
+        if (!carController)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 intersection = ray.origin + ray.direction * Mathf.Abs((ray.origin.y - projectileLauncher.transform.position.y) / ray.direction.y);
+            transform.forward = (intersection - transform.position).X0Z();
+        }
+        else
+        {
+            transform.rotation *= Quaternion.Euler(0, inputs.x, 0);
+        }
 
         if (IsDashing) return;
-
-        _isShooting = Input.GetMouseButton(0);
-
-        characterController.Move(inputs.X0Y().normalized * ((_isShooting ? speedMultiplierWhileShooting.Get() : 1.0f) * speed.Get() * Time.deltaTime));
+        
+        if (carController)
+            characterController.Move(transform.forward * (inputs.y * (_isShooting ? speedMultiplierWhileShooting.Get() : 1.0f) * speed.Get() * Time.deltaTime));
+        else
+            characterController.Move(inputs.X0Y().normalized * ((_isShooting ? speedMultiplierWhileShooting.Get() : 1.0f) * speed.Get() * Time.deltaTime));
         
         if (_isShooting)
         {
