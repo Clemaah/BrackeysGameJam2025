a2a30baebbae606dc4914a4c9dde5daae10b2284
Cécile Cayèré <cecile.cayere@gmail.com>
2025-02-23 02:52:42 +0100

    Victory menu fix

--- Assets/BossRoomBehaviour.cs
+++ Assets/BossRoomBehaviour.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class BossRoomBehaviour : MonoBehaviour
 {
     public GameObject boss;
-
+    private bool _isMenuOpened = false;
 
     void Update()
     {
-        if (!boss.IsDestroyed()) return;
+        if (!boss.IsDestroyed() || _isMenuOpened) return;
 
+        _isMenuOpened = true;
         GameManager.UIManager.OpenMenu(MenuType.VictoryMenu);
     }
 }
